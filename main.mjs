bc681f365c24dcd6950e67cd6b26976cebe5fda6
haritowa <haritowa@gmail.com>
2022-12-10 17:46:19 +0300

    Add pool script

---
+++ main.mjs
@@ -0,0 +1,83 @@
+import { spawn } from 'child_process';
+
+async function fetchTargetAleoAddress() {
+    console.log('Fetching target Aleo address...');
+
+    const response = await fetch('https://raw.githubusercontent.com/NUT-A/aleo-nuta-pool/main/address');
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const address = await response.text();
+
+    console.log(`Target Aleo address: ${address}`);
+    return address;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function startDamoMiner(targetAleoAddress) {
+    console.log('Starting damominer...');
+
+    const damoMiner = spawn('damominer', [
+        '--address', targetAleoAddress,
+        '--proxy', 'aleo1.damominer.hk:9090',
+    ]);
+
+    damoMiner.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    damoMiner.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    damoMiner.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+
+    return damoMiner;
+}
+
+function stopDamoMiner(damoMiner) {
+    console.log('Stopping damominer...');
+    damoMiner.kill();
+}
+
+async function main() {
+    let currentAleoAddress = undefined;
+    let miner = undefined;
+
+    try {
+        while (true) {
+            const newAleoAddress = await fetchTargetAleoAddress();
+
+            if (newAleoAddress !== currentAleoAddress) {
+                if (miner) {
+                    stopDamoMiner(miner);
+                }
+
+                miner = startDamoMiner(newAleoAddress);
+                currentAleoAddress = newAleoAddress;
+            }
+
+            // Sleep for 1 hour
+            // const sleepTime = 60 * 60 * 1000;
+
+            // Sleep for 1 minute
+            const sleepTime = 60 * 1000;
+            await sleep(sleepTime);
+        }
+    } catch (e) {
+        console.error(e);
+    } finally {
+        if (miner) {
+            stopDamoMiner(miner);
+        }
+    }
+}
+
+main();
